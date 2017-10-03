23e525609035b4e9d3077b8bfb035058b575e3c8
Zach Marshall <Zach.Marshall@sas.com>
2017-10-03 09:24:17 -0400

    Update for spring-boot 2

--- src/main/java/org/example/ExampleConfig.java
+++ src/main/java/org/example/ExampleConfig.java
@@ -2,9 +2,9 @@ package org.example;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
